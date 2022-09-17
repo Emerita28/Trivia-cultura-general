860813be26206e4a4fe0d3f0e091648af478e121
EMÉRITA CALLE <>
2022-09-17 00:11:51 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+print("Hola, Bienvenid@ a mi trivia")
+print("Descubre cuanto sabes de Machu Picchu")
+
+print("¿En qué año Machu Picchu fue elegida como una de las maravillas del mundo moderno?")
+# Seleciones la opción correcta 2007
+
+print("¿Qué significa Machu Picchu?")
+
+print("¿Cómo se llama el famoso cerro que está al costado de Machu Picchu?")
+
+print("¿En qué zonas está dividida la ciudadela de Machu Picchu?")
+
+print("¿Cuál era el idioma oficial de los incas?")
+
+print("¿A qué altitud promedio está la ciudadela?")
